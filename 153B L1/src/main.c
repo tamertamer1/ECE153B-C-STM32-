0b415352b34a82244166d6717c8f0fb8747c595e
tamertamer1 <63352729+tamertamer1@users.noreply.github.com>
2024-02-05 21:01:33 -0800

    Update main.c

--- 153B L1/src/main.c
+++ 153B L1/src/main.c
@@ -6,7 +6,7 @@
  * Lab: 1
  */
 #include "stm32l476xx.h"
-
+#include <stdbool.h>
 void Init(){
     // Enable HSI
     RCC->CR |= ((uint32_t)RCC_CR_HSION);
@@ -18,12 +18,19 @@ void Init(){
     while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) == 0 );
 
     // Enable GPIO Clock
+		RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+		RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
     // [TODO]
 
     // Initialize Green LED
+		GPIOA->MODER &= ~GPIO_MODER_MODE5_1;
+		GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
+		GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD5;
     // [TODO]
 
     // Initialize User Button
+		GPIOC->MODER &= ~GPIO_MODER_MODE13;
+		GPIOC->PUPDR &= ~GPIO_PUPDR_PUPD13;
     // [TODO]
 }
 
@@ -32,7 +39,26 @@ int main(void){
     Init();
 
     // Polling to Check for User Button Presses
-    // [TODO]
+		bool counter = true;
+	  int i;
+	  while (true){
+			for (i = 0; i<12000;i++){
+			}
+				
+		if ((GPIO_IDR_ID13 & GPIOC->IDR) == 0)
+		{
+			if (counter)
+			{counter = false;
+				GPIOA->ODR |= GPIO_ODR_OD5;
+				
+			}
+			else
+			{counter = true;
+				GPIOA->ODR &= ~GPIO_ODR_OD5;
+			}
+		}
 
+    // [TODO]
+	}
     return 0;
 }
